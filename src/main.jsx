475cb5e262b0acab461e33f8f987826e951d9b24
pablommm <murgiapablo@gmail.com>
2023-11-09 20:03:53 -0300

    Fix conflict

--- src/main.jsx
+++ src/main.jsx
@@ -3,26 +3,17 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 //import Home from './Home/Home.jsx'
 //import Login from './Login/Login.jsx'
-<<<<<<< Updated upstream
 import Home from './Home/Home.jsx'
 import Login from './Login/Login.jsx'
 import JugadorForm from './JugadorForm/JugadorForm.jsx'
-=======
-//import Plantilla from './Plantilla/Plantilla.jsx'
-import Figuritas from './Figuritas/Figuritas.jsx'
->>>>>>> Stashed changes
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Login /> */}
-<<<<<<< Updated upstream
-    <JugadorForm />
-=======
-    {/* <Home /> */}x
-    {/* <Plantilla /> */}
-    <Figuritas />
->>>>>>> Stashed changes
+     {/* <Home />  */}
+     <Plantilla /> 
+    {/* <Figuritas /> */}
   </React.StrictMode>,
 )
