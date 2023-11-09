247bb877a50d764a90d2d7efb4f742b6d7eb8bb9
Juani <juanignacioz1995@gmail.com>
2023-11-08 23:23:39 -0300

    se crea el primer componente react

---
+++ src/home.jsx
@@ -0,0 +1,33 @@
+import '../Maquetado/CSS/general.css'
+import '../Maquetado/CSS/login.css'
+import '../Maquetado/CSS/input.css'
+
+
+function Login() {
+    
+    return (    <div className="main-container">
+    <div className="sub-main-container">
+        <span className="login-title">WorldCApp</span>
+        <div className="login-item">
+            <label>Usuario</label>
+            <div className="login-info">
+                <i className="fa-solid fa-user login-icon"></i>
+                <input type="text" placeholder="Usuario" className="login-input"/> 
+            </div>
+        </div>
+
+        <div className="login-item">
+            <label>Contraseña</label>
+            <div className="login-info">
+                <i className="fa-solid fa-key login-icon"></i>
+                <input type="password" placeholder="Contraseña" className="login-input"/>
+            </div>
+        </div>
+        <div>
+            <button className="primary-button">Ingresar</button>
+        </div>
+    </div>
+</div>)
+  }
+  
+  export default Login
