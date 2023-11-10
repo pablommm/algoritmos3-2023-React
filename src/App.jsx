21e8b7db2d3dec9c4c1fa6345dcbb4720f061124
pablommm <murgiapablo@gmail.com>
2023-11-10 01:48:28 -0300

    se arregla componente SeleccionForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import SeleccionForm from './SeleccionForm/SeleccionForm'
-/* import Login from "./Login/Login"
+//import Login from "./Login/Login"
+ /*
 import Home from "./Home/Home"
 import JugadorForm from './JugadorForm/JugadorForm' */
 //import PuntoDeVentaForm from './PuntoDeVentaForm/PuntoDeVentaForm'
--- src/SeleccionForm/SeleccionForm.jsx
+++ src/SeleccionForm/SeleccionForm.jsx
@@ -1,39 +1,39 @@
-import '../../Maquetado/CSS/general.css'
-import '../../Maquetado/CSS/form.css'
 import '../../Maquetado/CSS/button.css'
+import '../../Maquetado/CSS/jugador.css'
 import '../../Maquetado/CSS/input.css'
+import '../../Maquetado/CSS/form.css'
+import '../../Maquetado/CSS/general.css'
 
-function SeleccionForm() {
-  
-  return (      
-  <section className="sub-main-container">
-      <div className="formulario">
+function Seleccion() {
+  return (
+    <div className="main-container">
+      <div className="sub-main-container">
+        <div className="formulario">
           <form action="p" method="POST">
-              <label htmlFor="nombre_jugador">Nombre del Jugador:</label>
-              <input type="text" name="nombre_jugador" required></input>
 
-              <label htmlFor="confederacion">Confederación:</label>
-              <select className="select">
-                  <option value="CONMEBOL">CONMEBOL</option>
-                  <option value="UEFA">UEFA</option>
-                  <option value="CAF">CAF</option>
-                  <option value="AFC">AFC</option>
-                  <option value="CONCACAF">CONCACAF</option>
-                  <option value="OFC">OFC</option>
-              </select>
+            <label htmlFor="nombre_jugador">Nombre del Jugador:</label>
+            <input type="text" name="nombre_jugador" required />
 
-              <label htmlFor="copas_mundo">Cantidad de Copas del Mundo:</label>
-              <input type="number" name="copas_mundo" required min="0"> </input>
+            <label htmlFor="confederacion">Confederación:</label>
+            <select className="select">
+              <option value="CONMEBOL">CONMEBOL</option>
+              <option value="UEFA">UEFA</option>
+              <option value="CAF">CAF</option>
+              <option value="AFC">AFC</option>
+              <option value="CONCACAF">CONCACAF</option>
+              <option value="OFC">OFC</option>
+            </select>
 
-              <button className="secondary-button">Volver</button>
-              <button className="primary-button">Guardar</button>
-        
-        </form>
+            <label htmlFor="copas_mundo">Cantidad de Copas del Mundo:</label>
+            <input type="number" name="copas_mundo" required min="0" />
+
+            <button className="secondary-button">Volver</button>
+            <button className="primary-button">Guardar</button>
+          </form>
+        </div>
       </div>
-  </section>
-  
+    </div>
   )
-  
 }
 
-export default SeleccionForm
+export default Seleccion
