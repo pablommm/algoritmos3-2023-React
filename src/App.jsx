4b17e6b366471cb1a7431d1ff56ccd3942543b2e
federico marquez <fedemv93@gmail.com>
2023-11-09 23:16:17 -0300

    eliminado de medida en media

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Login from "./Login/Login"
+import Home from "./Home/Home"
 function App() {
   return (
-    <div className="App">
+    <div className="Login">
       <Login />
-    </div >
+    </div>
+
 
 
   )
