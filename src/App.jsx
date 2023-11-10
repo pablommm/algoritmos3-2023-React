bcf23d19f6a863b00c3e992e8985b065ddf4c5f3
Juani <juanignacioz1995@gmail.com>
2023-11-10 02:06:34 -0300

    chakra

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css'
 import SeleccionForm from './SeleccionForm/SeleccionForm'
+import { ChakraProvider } from '@chakra-ui/react'
+
 //import Login from "./Login/Login"
- /*
-import Home from "./Home/Home"
-import JugadorForm from './JugadorForm/JugadorForm' */
-//import PuntoDeVentaForm from './PuntoDeVentaForm/PuntoDeVentaForm'
+/*
+import Home from "./Home/Home"*/
+import JugadorForm from './JugadorForm/JugadorForm'
+import PuntoDeVentaForm from './PuntoDeVentaForm/PuntoDeVentaForm'
 
 function App() {
   return (
-      <div className="SeleccionForm">
-       <SeleccionForm />
-     </div> 
+    <ChakraProvider>
+      <JugadorForm />
+    </ChakraProvider>
+  )
   /*
     <div>
       <SeleccionForm />
@@ -18,7 +21,7 @@ function App() {
 */
 
 
-  )
+
 }
 
 export default App
