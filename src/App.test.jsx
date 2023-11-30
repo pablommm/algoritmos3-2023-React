799a97a7e35f04ee2fa50978e79e50b96fdaeede
Juani <jzotelo@estudiantes.unsam.edu.ar>
2023-11-29 23:47:34 -0300

    test falopa

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,8 @@
+import { render} from '@testing-library/react'
+import Component from './components/Home/Home.jsx'
+
+/*
 import { render, screen, waitFor } from '@testing-library/react'
-import App from './App'
 
 describe('smoke test de la app', () => {
   test('la app levanta', async () => {
@@ -8,5 +11,20 @@ describe('smoke test de la app', () => {
     // solamente es para que no aparezca un mensaje de advertencia de usar act() 
     // para más info: https://javascript.plainenglish.io/you-probably-dont-need-act-in-your-react-tests-2a0bcd2ad65c
     await waitFor(() => { expect(screen.getByTestId('app')).toBeInTheDocument() })
+  })
+})
+
+*/
+
+describe('Componente', () => {
+  it('should validate the current page', () => {
+    // Renderiza el componente
+    const { getByText } = render(<Component />)
+
+    // Obtiene el elemento HTML para el título de la página
+    const titulo = getByText('Home')
+
+    // Verifica que el título de la página sea correcto
+    expect(titulo).toBeInTheDocument()
   })
 })
